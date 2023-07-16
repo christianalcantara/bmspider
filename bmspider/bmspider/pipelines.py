e193eec7acd1e502bafa1b121dbefa798449cf6a
christianalcantara <christian.douglas.alcantara@gmail.com>
2023-07-16 07:00:26 -0300

    :sparkles: pipeline to store data scraped in db

--- bmspider/bmspider/pipelines.py
+++ bmspider/bmspider/pipelines.py
@@ -5,9 +5,17 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+
+from .models import Author, Quote, QuoteTag, Tag, db
 
 
-class BmspiderPipeline:
-    def process_item(self, item, spider):
+class QuotesToScrapePipeline:
+    @staticmethod
+    def process_item(item, spider):
+        with db.atomic():
+            author, _ = Author.get_or_create(name=item["author"])
+            quote, _ = Quote.get_or_create(text=item["text"], author=author)
+            for name in item["tags"]:
+                tag, _ = Tag.get_or_create(name=name)
+                QuoteTag.get_or_create(quote=quote, tag=tag)
         return item
